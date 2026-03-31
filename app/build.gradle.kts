fab1def4a45e6b2ae7c31e5e14a5a68a8e196c58
sting1023 <128615011+sting1023@users.noreply.github.com>
2026-03-31 08:09:35 +0800

    chore: sync - app/build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
 
     defaultConfig {
         applicationId = "com.salarycalc"
-        minSdk = 1
+        minSdk = 26
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
